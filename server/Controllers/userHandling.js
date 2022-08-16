4966d3df9f93745a2e0b55632ca705ecd3ad6b9e
Pratik Pimpalkar <pratikpimpalkar21@gmail.com>
2022-08-16 15:26:55 +0530

    Logout Functionality done

--- server/Controllers/userHandling.js
+++ server/Controllers/userHandling.js
@@ -23,9 +23,31 @@ export const signin = async (req, res) => {
   try {
     await user.save();
     console.log("Registered Succesfully");
-    return res.status(200).send("Registration Succesful");
+    return res.status(200).send("Registration Succesfull");
   } catch (error) {
     console.log("Registration Failed: " + error);
     return res.status(200).send("Registration Failed");
   }
 };
+
+export const login = async (req, res) => {
+  const { name, password } = req.body;
+  try {
+    const user = await User.findOne({ name });
+    if (!user)
+      return res.status(400).send("User not found");
+    const match = await comparePassword(password, user.password)
+    if (!match)
+      return res.status(400).send("Please enter valid credentials")
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, { expiresIn: "3d" })
+    user.password = undefined;
+    user.cpassword = undefined;
+    res.status(200).json({
+      token, user
+    })
+
+  } catch (error) {
+    console.log(error);
+    return res.status(400).send(error);
+  }
+}
--- server/Routes/auth.js
+++ server/Routes/auth.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { signin } from '../Controllers/userHandling.js';
+import { signin ,login} from '../Controllers/userHandling.js';
 
 const router=express.Router();
 
-router.post('/signin',signin);
+router.post('/signin',signin); 
+router.post('/login',login); 
 
 export default router
